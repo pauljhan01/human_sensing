ab64365f3e81e2dbddbaaf805bf101cb480f0c6a
Paul J. Han <pauljhan@utexas.edu>
2026-03-02 23:44:32 -0600

    HW3: Complete

--- hw3/q1.py
+++ hw3/q1.py
@@ -8,8 +8,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, LeaveOneGroupOut, StratifiedKFold, StratifiedGroupKFold, cross_val_score, cross_val_predict
 from sklearn.metrics import classification_report
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import LeaveOneGroupOut
 
 class Gestures():
     def __init__(self, user, gesture, path):
@@ -25,20 +27,18 @@ class Gestures():
         self.image = image
 
     def apply_preprocessing(self):
-        # self.filtered_image = ski.filters.roberts(self.image)
-        # self.auto = ski.filters.rank.autolevel(self.image.astype(np.uint16), ski.morphology.disk(20))
-        # self.filtered_image = ski.filters.rank.enhance_contrast(self.image, ski.morphology.disk(5))
         self.image = ski.exposure.equalize_adapthist(self.image)
         self.image_resized = ski.transform.resize(self.image, (64, 64), anti_aliasing=True)
         self.image_hist = ski.feature.hog(self.image_resized, orientations=9, pixels_per_cell=(8, 8),
                                cells_per_block=(2, 2), visualize=False)
 
 data = []
+groups = []
 labels = []
+subjects = ["00", "01", "02", "03", "04"]
+gestures = ["01_palm", "02_l", "03_fist", "04_fist_moved", "05_thumb", "06_index", "07_ok", "08_palm_moved", "09_c", "10_down"]
 
 def load_data():
-    subjects = ["00", "01", "02", "03","04"]
-    gestures = ["01_palm", "02_l", "03_fist", "04_fist_moved", "05_thumb", "06_index", "07_ok", "08_palm_moved", "09_c", "10_down"]
     # subjects = ["00"]
     # gestures = ["01_palm"]
 
@@ -49,30 +49,50 @@ def load_data():
                 name = "".join(gesture.split("_")[1:])
                 full_path = path + f"{file}"
                 image = Gestures(subject, name, full_path)
-                data.append(image.image_resized)
+                data.append(image.image_hist)
+                groups.append(int(subject[1]))
                 labels.append(name)
 
 load_data()
-
+groups = np.array(groups)
 X = data
 y = labels
-
 labeler = LabelEncoder()
 y = labeler.fit_transform(y)
-
+y = np.array(y)
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
+X = np.array(X)
 
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2001)
+# pipeline = Pipeline([
+#     # ('label', LabelEncoder()),
+#     ('scale', StandardScaler()),
+#     ('svc', SVC(kernel='rbf', C=10, gamma='scale'))
+# ])
 
-clf = SVC(kernel='rbf', C=10, gamma='scale')
-clf.fit(x_train, y_train)
+svc = SVC(kernel='rbf', C=10, gamma='scale')
 
-y_pred = clf.predict(x_test)
-print(classification_report(y_test, y_pred))
+logo = LeaveOneGroupOut()
+y_pred = cross_val_predict(svc, X, y, groups=groups, cv=logo)
+print(classification_report(y, y_pred=y_pred))
 
-# for image in data:
-#     plt.figure()
-#     plt.imshow(image.filtered_image, cmap="gray")
-#     plt.savefig("test")
-#     print(image.path)
+cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=2001)
+y_pred = cross_val_predict(svc, X, y, cv=cv)
+print(classification_report(y, y_pred))
+
+for subject in subjects:
+    X_subject = X[groups == int(subject[1])]
+    y_subject = y[groups == int(subject[1])]
+
+    cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=2001)
+
+    cv_score = cross_val_score(svc, X_subject, y_subject, cv=cv)
+    print(cv_score)
+
+# x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2001)
+
+# clf = SVC(kernel='rbf', C=10, gamma='scale')
+# clf.fit(x_train, y_train)
+
+# y_pred = clf.predict(x_test)
+# print(classification_report(y_test, y_pred))
